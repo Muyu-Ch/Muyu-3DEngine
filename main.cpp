6a26b14adbe30f706f2d58c8f658ab22f63237b3
muyu <1744391048@qq.com>
2026-03-15 22:19:53 +0800

    优化了坐标存储方式

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // main.cpp（测试入口）
 #include "Render.h"
 #include <iostream>
+#include<vector>
 
 int main()
 {
@@ -15,16 +16,20 @@ int main()
     }
 
     bool isRunning = true;    // 主循环开关
-    SDL_Event event;          // 事件对象（检测按键/关闭窗口）
-    Vector3 point1(300,300,10);
-    Vector3 point2(300,-300,10);
-    Vector3 point3(-300,300,10);
-    Vector3 point4(-300,-300,10);
-    Vector3 point5(300,300,310);
-    Vector3 point6(300,-300,310);
-    Vector3 point7(-300,300,310);
-    Vector3 point8(-300,-300,310);
-    float Dz=1;
+    SDL_Event event; // 事件对象（检测按键/关闭窗口）
+
+    std::vector<Vector3> vectors={
+        Vector3(300,300,10),
+        Vector3(300,-300,10),
+        Vector3 (-300,300,10),
+        Vector3(-300,-300,10),
+        Vector3(300,300,310),
+        Vector3(300,-300,310),
+        Vector3(-300,300,310),
+        Vector3(-300,-300,310)
+    };
+
+     float Dz=1;
 
     while (isRunning)
     {
@@ -36,47 +41,34 @@ int main()
                 isRunning = false;
         }
 
-        point1.z+=Dz;
-        point2.z+=Dz;
-        point3.z+=Dz;
-        point4.z+=Dz;
-        point5.z+=Dz;
-        point6.z+=Dz;
-        point7.z+=Dz;
-        point8.z+=Dz;
+
+        for (size_t i=0;i<vectors.size();i++)
+        {
+            vectors[i].z += Dz;
+        }
 
         render.Clear();
 
-        int screenX1, screenY1;
-        int screenX2, screenY2;
-        int screenX3, screenY3;
-        int screenX4, screenY4;
-        int screenX5, screenY5;
-        int screenX6, screenY6;
-        int screenX7, screenY7;
-        int screenX8, screenY8;
-        render.Project(point1, screenX1, screenY1);
-        render.Project(point2, screenX2, screenY2);
-        render.Project(point3, screenX3, screenY3);
-        render.Project(point4, screenX4, screenY4);
-        render.Project(point5, screenX5, screenY5);
-        render.Project(point6, screenX6, screenY6);
-        render.Project(point7, screenX7, screenY7);
-        render.Project(point8, screenX8, screenY8);
+        std::vector<std::pair<int,int>>points;
+
+        for (int i=0;i<8;i++)
+        {
+            int x,y;
+            render.Project(vectors[i],x,y);
+            points.push_back(std::make_pair(x,y));
+        }
 
         // 画一个5x5的白色方块（方便看）
-        for (int dx = -4; dx <= 4; dx++)
+        for (int i=0;i<8;i++)
         {
-            for (int dy = -4; dy <= 4; dy++)
+            int x=points[i].first;
+            int y=points[i].second;
+            for (int dx = -4; dx <= 4; dx++)
             {
-                render.DrawPixel(screenX1 + dx, screenY1 + dy, 255, 255, 255);
-                render.DrawPixel(screenX2 + dx, screenY2 + dy, 255, 255, 255);
-                render.DrawPixel(screenX3 + dx, screenY3 + dy, 255, 255, 255);
-                render.DrawPixel(screenX4 + dx, screenY4 + dy, 255, 255, 255);
-                render.DrawPixel(screenX5 + dx, screenY5 + dy, 255, 255, 255);
-                render.DrawPixel(screenX6 + dx, screenY6 + dy, 255, 255, 255);
-                render.DrawPixel(screenX7 + dx, screenY7 + dy, 255, 255, 255);
-                render.DrawPixel(screenX8 + dx, screenY8 + dy, 255, 255, 255);
+                for (int dy = -4; dy <= 4; dy++)
+                {
+                    render.DrawPixel(x+dx,y+dy);
+                }
             }
         }
 
