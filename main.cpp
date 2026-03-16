dccea838ba7975fc8afe0c70fce7b60d7fad8c24
muyu <1744391048@qq.com>
2026-03-16 14:08:08 +0800

    实现了一个正方体在原地的旋转

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include <iostream>
 #include<vector>
 
+#include "Matrix4.h"
+
 int main()
 {
     // 创建Render对象，默认800x600窗口
@@ -19,17 +21,18 @@ int main()
     SDL_Event event; // 事件对象（检测按键/关闭窗口）
 
     std::vector<Vector3> vectors={
-        Vector3(300,300,10),
-        Vector3(300,-300,10),
-        Vector3 (-300,300,10),
-        Vector3(-300,-300,10),
-        Vector3(300,300,310),
-        Vector3(300,-300,310),
-        Vector3(-300,300,310),
-        Vector3(-300,-300,310)
+        Vector3(150,150,300),
+        Vector3(150,-150,300),
+        Vector3 (-150,-150,300),
+        Vector3(-150,150,300),
+        Vector3(150,150,600),
+        Vector3(150,-150,600),
+        Vector3(-150,-150,600),
+        Vector3(-150,150,600)
     };
 
-     float Dz=1;
+    const Vector3 cubeCenter(0.0f, 0.0f, 450.0f);
+    float rotateAngle=1;
 
     while (isRunning)
     {
@@ -41,10 +44,17 @@ int main()
                 isRunning = false;
         }
 
+        Matrix4 translateToOrigin = Matrix4::Translate(-cubeCenter.x, -cubeCenter.y, -cubeCenter.z);
+        // 步骤2：绕Y轴旋转矩阵（角度逐步增加）
+        Matrix4 rotateY = Matrix4::RotateY(rotateAngle);
+        // 步骤3：平移矩阵 - 把立方体移回原位置
+        Matrix4 translateBack = Matrix4::Translate(cubeCenter.x, cubeCenter.y, cubeCenter.z);
+
+        Matrix4 transform = translateBack.Multiply(rotateY.Multiply(translateToOrigin));
 
         for (size_t i=0;i<vectors.size();i++)
         {
-            vectors[i].z += Dz;
+            vectors[i]=transform.MultiplyVector(vectors[i]);
         }
 
         render.Clear();
@@ -59,17 +69,24 @@ int main()
         }
 
         // 画一个5x5的白色方块（方便看）
-        for (int i=0;i<8;i++)
+        // for (int i=0;i<8;i++)
+        // {
+        //     int x=points[i].first;
+        //     int y=points[i].second;
+        //     for (int dx = -4; dx <= 4; dx++)
+        //     {
+        //         for (int dy = -4; dy <= 4; dy++)
+        //         {
+        //             render.DrawPixel(x+dx,y+dy);
+        //         }
+        //     }
+        // }
+
+        for (int i=0;i<4;i++)
         {
-            int x=points[i].first;
-            int y=points[i].second;
-            for (int dx = -4; dx <= 4; dx++)
-            {
-                for (int dy = -4; dy <= 4; dy++)
-                {
-                    render.DrawPixel(x+dx,y+dy);
-                }
-            }
+            render.DrawLine(points[i].first,points[i].second,points[(i+1)%4].first,points[(i+1)%4].second);
+            render.DrawLine(points[i+4].first,points[i+4].second,points[(i+1)%4+4].first,points[(i+1)%4+4].second);
+            render.DrawLine(points[i].first,points[i].second,points[i+4].first,points[i+4].second);
         }
 
         // 5. 刷新显示（把新画面展示到窗口）
